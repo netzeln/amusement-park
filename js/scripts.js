0737cb9e537733e3f2c5fbac26885bee645f0124
Nic Netzel and torrence stratton <tlstratton@gmail.com>
2016-01-13 09:36:52 -0800

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+  var height = parseInt(prompt("What's your Height in Inches?"));
 
-
-
+  if (height >= 63) {
+    $(".tall").show();
+  } else {
+    $(".short").show();
+  }
+  
 });
